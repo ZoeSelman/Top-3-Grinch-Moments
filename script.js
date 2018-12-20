22ee9c216af88d21e1031f81ba0fa23edfb5c145
ZoeSelman <43343194+ZoeSelman@users.noreply.github.com>
2018-12-20 09:15:02 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+$(".baby").click(function(){
+$("#BabyGrinch").slideToggle ();
+});
+
+$(".cunning").click(function(){
+$(".move").fadeToggle ();
+});
+
+$(".angry").click(function(){
+$(".meanOnee").slideUp ();
+$(".meanOnee").slideDown ();
+});
+
+$(".surprised").click(function(){
+$(".meanTwo").fadeOut ();
+$(".meanTwo").slideDown ();
+});
+
